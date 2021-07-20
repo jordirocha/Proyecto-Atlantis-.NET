24c13cbbee9b23252de46e33a294862fd09a711f
Daniel Vela <danivelam12@gmail.com>
2021-07-20 17:47:40 +0200

    Dia 20 (Jordi+Dani)
    Login + Inicio

---
+++ WebApplication1/Usuario.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApplication1
+{
+    internal class Usuario
+    {
+
+        int id;
+        string nombre;
+        string email;
+        string tipoPermiso;
+
+        public int Id { get => id; set => id = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Email { get => email; set => email = value; }
+        public string TipoPermiso { get => tipoPermiso; set => tipoPermiso = value; }
+    }
+}
